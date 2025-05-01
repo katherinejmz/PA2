10c83ff47385163182ff3ffe1f20c24d6a08eb29
Nicu Balica <nbalcia@myges.fr>
2025-05-01 12:28:38 +0200

    Initialisation des projets FrontOffice et BackOffice

---
+++ frontoffice/src/pages/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div className="h-screen flex items-center justify-center bg-green-100">
+        <h1 className="text-3xl font-bold text-green-700">Bienvenue sur EcoDeli FrontOffice</h1>
+      </div>
+    );
+  }
+  export default Home;
+  
